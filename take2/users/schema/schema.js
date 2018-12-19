c09b501bc49d3c1a4b6510692a06a1b58dab3a67
parenparen <dtmuelle@gmail.com>
2018-12-19 10:45:18 -0800

    wip addded graphiql

--- take2/users/schema/schema.js
+++ take2/users/schema/schema.js
@@ -2,8 +2,14 @@ const graphql = require('graphql');
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLSchema,
+  GraphQLInt,
 } = graphql;
+
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '47', firstName: 'Samantha', age: 21 },
+];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -14,3 +20,19 @@ const UserType = new GraphQLObjectType({
   }
 });
 
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
